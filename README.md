0566375ad14eab15452422e1df8cb8689faec998
Matt Carter <m@ttcarter.com>
2012-05-01 16:09:05 +1000

    Upgraded to always use the .js file rather than making it optional Added support for local and global navbars Added global and page level option support Added demo page and files BUGFIX: Picturenav behaviour now no longer throws an error when picturenav is not being used BUGFIX: Navbars now correcly adjust the height of the app page when visible

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 jQTouch-Kitchen-Sink
 ====================
 A collection of useful styles and scripts for use in jQTouch.
+
+[Kitchen Sink Demo](demo.html)
+
+
+API reference
+=============
+
+Global settings / The JQT element
+---------------------------------
+The #jqt represents the main jQTouch wrapper. All global kitchen-sink options can be applied to this element for use later.
+
+	<div id="jqt" data-iconbase="/img/icons/" data-iconsuffix=".png">
+		// ... App content ... //
+	</div>
+
+In the above example the option 'iconbase' and 'iconsuffix' options are set to the corresponding value.
+
+Options:
+* __iconbase__ - Specify a prefix or base directory of where to look for icons. Can be used with 'iconsuffix' for greater functionality.
+* __iconsuffix__ - Automatically suffix all icons with this option (e.g. '.png')
+
+
+Page level settings
+-------------------
+The following options are specified in much the same way as the global settings but effect only that page
+
+	<div id="jqt">
+		<div id="home" data-navbar="show">
+			// ... Page content ... //
+		</div>
+	</div>
+
+In the above example the option 'navbar' is set to the corresponding value.
+
+Options:
+* __navbar__ - Specify whether the global navbar should be shown on this page. Values are: 'show', 'hide' and 'keep'. If a local navbar is present it will automatically replace the global one.
--- kitchen-sink.js
+++ kitchen-sink.js
@@ -1,14 +1,79 @@
 $(function() {
-	$('.picturenav').bind('swipe', function(e, d) {
-		var direction = (d && d.direction) || 'left';
-		var offset = $(this).data('offset') || 0;
-		if (direction == 'left') {
-			offset = (offset+1 > $(this).find('li').length-1) ? 0 : offset+1;
-		} else
-			offset = (offset-1 < 0) ? $(this).find('li').length-1 : offset-1;
-		$(this).data('offset', offset);
+/* A collection of useful jQTouch style addons */
+/* Author: Matt Carter <m@ttcarter.com> */
+/* Source: https://github.com/hash-bang/jqtouch-kitchen-sink */
 
-		console.log('show ' + offset);
-		$(this).css('background-image', 'url(' + $(this).find('li > img').eq(offset).attr('src') + ')');
-	}).swipe();
+// Global options {{{
+$.extend({kitchensink: {
+	iconbase: $('#jqt').data('iconbase'),
+	iconsuffix: $('#jqt').data('iconsuffix')
+}});
+// }}}
+// Icons {{{
+$('#jqt .icons > li[data-icon]').each(function() {
+	$(this).css('background-image', 'url(' + $.kitchensink.iconbase + $(this).data('icon') + $.kitchensink.iconsuffix + ')');
+});
+// }}}
+// Articles {{{
+// }}}
+// Post {{{
+// No JS needed
+// }}}
+// Picturenav {{{
+$('.picturenav').bind('swipe', function(e, d) {
+	var direction = (d && d.direction) || 'left';
+	var offset = $(this).data('offset') || 0;
+	if (direction == 'left') {
+		offset = (offset+1 > $(this).find('li').length-1) ? 0 : offset+1;
+	} else
+		offset = (offset-1 < 0) ? $(this).find('li').length-1 : offset-1;
+	$(this).data('offset', offset);
+	$(this).css('background-image', 'url(' + $(this).find('li > img').eq(offset).attr('src') + ')');
+}).trigger('swipe');
+// }}}
+// Navbar {{{
+$('#jqt .navbar > ul > li[data-icon], #jqt > .global-navbar > ul > li[data-icon]').each(function() {
+	$(this).css('background-image', 'url(' + $.kitchensink.iconbase + $(this).data('icon') + $.kitchensink.iconsuffix + ')');
+});
+// }}}
+// Global-Navbar {{{
+if ($('#jqt > .global-navbar').length) { // If there is a global navbar defined
+	$.kitchensink.navbar = $('#jqt > .global-navbar');
+	$('#jqt').before($.kitchensink.navbar); // Move this element to be a peer of #jqt
+}
+$('#jqt > div').bind('pageAnimationEnd', function(e, info) { // Install page change listener
+	if (info.direction == 'out') return; // Ignore outgoing pages
+
+	var navbarshow;
+	if ($(this).find('.navbar').length) { // This page has its own navbar
+		navbarshow = 'local'; // Force the global setting to use the local
+	} else if ($.kitchensink.navbar) { // Not specified but there is a global navbar
+		navbarshow = $(this).data('navbar') || 'show'; // Ask the page what it wants to do
+	} else
+		navbarshow = 'hide';
+
+	if ($.kitchensink.navbarlocal) { // Local navbar in use - destroy
+		$.kitchensink.navbarlocal.remove();
+		$.kitchensink.navbarlocal = 0;
+	}
+
+	if (navbarshow) {
+		if ($.kitchensink.navbar && navbarshow == 'hide') {
+			$.kitchensink.navbar.hide();
+		} else if (navbarshow == 'show' || navbarshow == 'local') { // Show something - either local or global
+			if (navbarshow == 'local') { // Replace with local
+				if ($.kitchensink.navbar)
+					$.kitchensink.navbar.hide();
+				$.kitchensink.navbarlocal = $(this).find('.navbar').clone();
+				$('#jqt').before($.kitchensink.navbarlocal);
+			} else if (navbarshow == 'show') {
+				$.kitchensink.navbar.show();
+			}
+			console.log('Add padding to ' + $(this).attr('id'));
+			$(this).addClass('navbar-padding');
+		}
+	}
+});
+// }}}
+
 });
